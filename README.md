180d4c9ad1d433cf89d776a4e3c15c4a4923f4fe
zuoyuanxun <975978920@qq.com>
2020-08-18 17:46:02 +0800

    zookeeper链接测试

--- README.md
+++ README.md
@@ -2,3 +2,25 @@
 
 ## 整合mybatis
 要在application.properties加上xml的地址
+
+# zookeeper
+## maven 依赖：
+`        <dependency>
+             <groupId>org.apache.zookeeper</groupId>
+             <artifactId>zookeeper</artifactId>
+             <version>3.6.1</version>
+             <!--排除这个slf4j-log4j12-->
+             <exclusions>
+                 <exclusion>
+                     <groupId>org.slf4j</groupId>
+                     <artifactId>slf4j-log4j12</artifactId>
+                 </exclusion>
+             </exclusions>
+         </dependency>`
+## 问题
+### 新版的zookeeper开启会占用8080端口<br>
+解决方法:<br>
+在conf/zoo.cfg中添加：<br>
+admin.serverPort=没有使用的端口
+### 要排除zookeeper中的slf4j不然会冲突
+
---
+++ api/src/test/java/com/example/api/zookeeper/ZookeeperTest.java
@@ -0,0 +1,61 @@
+package com.example.api.zookeeper;
+
+import org.apache.zookeeper.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+
+@SpringBootTest
+public class ZookeeperTest {
+
+    @Test
+    public void zookeeperTest() throws KeeperException, InterruptedException, IOException {
+        String connStr = "127.0.0.1:2181";
+        CountDownLatch countDown = new CountDownLatch(1);
+        Watcher watcher = event -> {
+            if (event.getState() == Watcher.Event.KeeperState.SyncConnected) {
+                System.out.println("eventType:" + event.getType());
+                if (event.getType() == Watcher.Event.EventType.None) {
+                    countDown.countDown();
+                } else if (event.getType() == Watcher.Event.EventType.NodeCreated) {
+                    System.out.println("listen:节点创建");
+                } else if (event.getType() == Watcher.Event.EventType.NodeChildrenChanged) {
+                    System.out.println("listen:子节点修改");
+                }
+            }
+        };
+        ZooKeeper zookeeper = new ZooKeeper(connStr, 50000, watcher);
+        countDown.await();
+
+        String zNode = "/Zuo6";
+        //注册监听,每次都要重新注册，否则监听不到
+        zookeeper.exists(zNode, watcher);
+
+        // 创建节点
+        String result = zookeeper.create(zNode, "一生一世".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+        System.out.println(result);
+
+        Thread.sleep(10);
+
+        // 获取节点
+        //按字节存取
+        byte[] bs = zookeeper.getData(zNode, true, null);
+        result = new String(bs);
+        System.out.println("创建节点后的数据是:" + result);
+
+        // 修改节点
+        zookeeper.setData(zNode, "I love you".getBytes(), -1);
+
+        Thread.sleep(10);
+
+        bs = zookeeper.getData(zNode, true, null);
+        result = new String(bs);
+        System.out.println("修改节点后的数据是:" + result);
+
+        // 删除节点
+        zookeeper.delete(zNode, -1);
+        System.out.println("节点删除成功");
+    }
+}
